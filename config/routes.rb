6644822aa868861857f57eee43a49b588474a85d
github username <github email address>
2019-05-10 00:41:26 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'products/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
+  get '/', to: 'products#add'
 end
