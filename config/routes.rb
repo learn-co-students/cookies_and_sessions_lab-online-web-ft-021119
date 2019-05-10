1f82ddc44073d50c3134cd134845e264cad1beed
AudreyJones <audrey.eliz.jones@gmail.com>
2019-05-10 17:44:50 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # one to display the Products#index (the root route)
+  root 'products#index'
+
+  # and one to post the products to add them to the cart.
+  patch '/add', to: 'products#add'
 end
