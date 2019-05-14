177b31e5c91bb022771cc6c54c14780f24db5ce8
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-14 13:15:56 -0500

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+   
+    def index
+     render :index
+    end
+
+    def add
+        cart << params[:product]
+        render :index
+    end
+
+end
